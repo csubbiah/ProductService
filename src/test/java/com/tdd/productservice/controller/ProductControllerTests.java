2f833e76e4421f6aa3caa259df0fa773ccb4d870
Subbiah Chockalingam <subbiah.chockalingam@live.com>
2024-09-10 09:08:58 -0400

    TDD Controller - Iteration 1

--- src/test/java/com/tdd/productservice/controller/ProductControllerTests.java
+++ src/test/java/com/tdd/productservice/controller/ProductControllerTests.java
@@ -1,12 +1,15 @@
 package com.tdd.productservice.controller;
 
+
+
 import org.hamcrest.Matchers;
-import org.junit.jupiter.api.Assertions;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.web.servlet.MockMvc;
+
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
@@ -18,7 +21,10 @@ class ProductControllerTests {
     private MockMvc mockMvc;
 
     @Test
-    void givenAProductWhen_NameisnotnullAndPricegreaterThanZeroAndQuantityGreaterThanZero_ThenAddProduct() throws Exception {
+    void givenAProductSearchWhen_NameisnotnullAndPricegreaterThanZeroAndQuantityGreaterThanZero_ThenAddProduct() throws Exception {
+        // Validate if product is not null
+
+        // Generate Assertion to check if product is returned successfully
         mockMvc.perform(MockMvcRequestBuilders.get("/products"))
                 .andExpect(MockMvcResultMatchers.status().isOk())
                 .andExpect(MockMvcResultMatchers.jsonPath("$.productName", Matchers.is("Test Product")))
@@ -27,7 +33,10 @@ class ProductControllerTests {
     }
 
     @Test
-    void givenAProductWhen_NameisNull_ThenReturnBadRequest() {
-        Assertions.fail("Product name is required");
+    void givenAProductSearchWhen_NameisNull_ThenReturnBadRequest() throws Exception {
+        //Generate Bad request exception using MockMVCResultWatchers
+         mockMvc.perform(MockMvcRequestBuilders.get("/products"))
+                .andExpect(MockMvcResultMatchers.status().isBadRequest());
+
     }
 }
