4524184fe6c895ff885d53ef27c4c696844a6f18
Subbiah Chockalingam <subbiah.chockalingam@live.com>
2024-09-25 21:17:33 -0400

    searchProducts functionality added

--- src/main/java/com/tdd/productservice/controller/ProductController.java
+++ src/main/java/com/tdd/productservice/controller/ProductController.java
@@ -6,11 +6,11 @@ import com.tdd.productservice.service.IProductService;
 
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.validation.Valid;
+
 import java.util.List;
 import java.util.Optional;
 
@@ -32,7 +32,7 @@ public class ProductController {
     }
 
     @GetMapping("/search")
-    public Optional<List<Product>> searchProducts(@Valid @RequestBody Product product) {
+    public Optional<List<Product>> searchProducts(Product product) {
 
         return productService.searchProducts(product);
     }
